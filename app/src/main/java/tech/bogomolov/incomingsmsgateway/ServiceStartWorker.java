6a96de2112792d3721c2d3da29c09ca79cf5ca99
kobibest <kobi@eitan-m.co.il>
2025-12-17 12:05:41 +0200

    Implement ServiceStartWorker for SMS handling

---
+++ app/src/main/java/tech/bogomolov/incomingsmsgateway/ServiceStartWorker.java
@@ -0,0 +1,35 @@
+package tech.bogomolov.incomingsmsgateway;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class ServiceStartWorker extends Worker {
+
+    public ServiceStartWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters params
+    ) {
+        super(context, params);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        Context context = getApplicationContext();
+        Intent intent = new Intent(context, SmsReceiverService.class);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent);
+        } else {
+            context.startService(intent);
+        }
+
+        return Result.success();
+    }
+}
