81f7213bf4f73b206af7a898bdbe365902e5e10b
kobibest <kobi@eitan-m.co.il>
2025-12-17 12:04:56 +0200

    Refactor BootCompletedReceiver to use WorkManager

--- app/src/main/java/tech/bogomolov/incomingsmsgateway/BootCompletedReceiver.java
+++ app/src/main/java/tech/bogomolov/incomingsmsgateway/BootCompletedReceiver.java
@@ -1,20 +1,28 @@
 package tech.bogomolov.incomingsmsgateway;
 
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+
     @Override
-    public void onReceive(Context context, Intent argIntent) {
-        Intent intent = new Intent(context, SmsReceiverService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(intent);
-        } else {
-            context.startService(intent);
-        }
+    public void onReceive(Context context, Intent intent) {
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+
+        OneTimeWorkRequest workRequest =
+                new OneTimeWorkRequest.Builder(ServiceStartWorker.class)
+                        .setConstraints(constraints)
+                        .build();
+
+        WorkManager.getInstance(context).enqueue(workRequest);
     }
 }
